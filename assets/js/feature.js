b49af79fe01907d104be2e8386b2930b61152c49
Zwogli <Mathiaskohler@web.de>
2023-04-08 09:26:56 +0200

    add main margin

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -1,5 +1,7 @@
 function searchbarToggle(){
- let searchbar = document.getElementById('searchbar');
+ let  searchbar = document.getElementById('searchbar'),
+      main = document.getElementById('main'); 
 
  searchbar.classList.toggle('d-flex');
+ main.classList.toggle('main-mtop');
 }
