a6f8aa9fd9d7d8c37715c00723c488e0d8aaf8e6
Zwogli <Mathiaskohler@web.de>
2023-04-08 13:40:32 +0200

    edit space

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -1,7 +1,7 @@
 function searchbarToggle(){
  let  searchbar = document.getElementById('searchbar'),
-      main = document.getElementById('main'); 
+      main = document.getElementById('main');
 
- searchbar.classList.toggle('d-flex');
- main.classList.toggle('main-mtop');
+  searchbar.classList.toggle('d-flex');
+  main.classList.toggle('main-mtop');
 }
