7368c45c7209a3644c8ff787f93ef47b685cbfba
Zwogli <Mathiaskohler@web.de>
2023-06-05 09:53:02 +0200

    add cursor style

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,6 +1,6 @@
 function generateHtmlMainPagePokemon(pokemon, i, pokemonId){
   document.getElementById('main-section').innerHTML += /*html*/`
-    <div id="main_pokemon_${i}" onclick="toggleOverlay(), renderOverlayCard(${i})" class="main-pokemon">  
+    <div id="main_pokemon_${i}" onclick="toggleOverlay(), renderOverlayCard(${i})" class="main-pokemon c-pointer">  
       <h1 class="main-pokemon-name">${pokemon['name']}</h1>
       <div class="main-pokemon-infos">
         <div class="main-pokemon-infos-id_typ">
@@ -37,11 +37,11 @@ function generatePokemonCard(pokemon, i, pokemonId){
 
   pokemonCard.innerHTML = /*html*/`
     <div id="overlay_pokemon_${i}" onclick="event.stopPropagation()" class="overlay-pokemon">  
-      <h1 class="main-pokemon-name overlay-pokemon-name">${pokemon['name']}</h1>
+      <h1 class="main-pokemon-name overlay-pokemon-name c-default">${pokemon['name']}</h1>
       <div class="main-pokemon-infos overlay-pokemon-infos">
         <div class="main-pokemon-infos-id_typ">
-          <span class="main-pokemon-infos-id">#${pokemonId}</span>
-          <div id="overlay_pokemon_typ-${i}" class="main-pokemon-infos-typs  overlay-pokemon-infos-typ"></div>
+          <span class="main-pokemon-infos-id c-default">#${pokemonId}</span>
+          <div id="overlay_pokemon_typ-${i}" class="main-pokemon-infos-typs overlay-pokemon-infos-typ c-default"></div>
         </div>
         <div class="main-pokemon-img-container">
           <img class="main-pokemon-img overlay-pokemon-img" src="${pokemon['sprites'].other['official-artwork']['front_default']}">
@@ -51,17 +51,17 @@ function generatePokemonCard(pokemon, i, pokemonId){
     </div>
     <div id="overlay-pokemon-more" onclick="event.stopPropagation()" class="overlay-pokemon-more">
       <div class="overlay-pokemon-more-headline">
-        <img onclick="overlayPokemonGallery(-1, ${i})" class="overlay-pokemon-more-headline-arrow" src="assets/img/icon/arrow_left.png" alt="arrow left">
-        <span id="about-headline${i}" onclick="overlayMoreSection('about-', ${i})" class="">About</span>
-        <span id="baseStat-headline${i}" onclick="overlayMoreSection('baseStat-', ${i})" class="fw-bold">Base Stat</span>
-        <span id="moves-headline${i}" onclick="overlayMoreSection('moves-', ${i})" class="">Moves</span>
-        <img onclick="overlayPokemonGallery(1, ${i})" class="overlay-pokemon-more-headline-arrow" src="assets/img/icon/arrow_right.png" alt="arrow right">
+        <img onclick="overlayPokemonGallery(-1, ${i})" class="overlay-pokemon-more-headline-arrow c-pointer" src="assets/img/icon/arrow_left.png" alt="arrow left">
+        <span id="about-headline${i}" onclick="overlayMoreSection('about-', ${i})" class="c-pointer">About</span>
+        <span id="baseStat-headline${i}" onclick="overlayMoreSection('baseStat-', ${i})" class="fw-bold c-pointer">Base Stat</span>
+        <span id="moves-headline${i}" onclick="overlayMoreSection('moves-', ${i})" class="c-pointer">Moves</span>
+        <img onclick="overlayPokemonGallery(1, ${i})" class="overlay-pokemon-more-headline-arrow c-pointer" src="assets/img/icon/arrow_right.png" alt="arrow right">
       </div>
 
       <div class="overlay-pokemon-infos">
-        <div id="about-${i}" class="overlay-pokemon-infos-about d-none"></div>
-        <div id="baseStat-${i}" class=""></div>
-        <div id="moves-${i}" class="overlay-pokemon-infos-moves d-none" ></div>
+        <div id="about-${i}" class="overlay-pokemon-infos-about d-none c-default"></div>
+        <div id="baseStat-${i}" class="c-default"></div>
+        <div id="moves-${i}" class="overlay-pokemon-infos-moves d-none c-default" ></div>
       </div>
 
     </div>
